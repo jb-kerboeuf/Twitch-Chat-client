54341b800179ac65a1ff2836533c568c3c9167fa
jb-kerboeuf <ambre@MacBook-Air.local>
2025-06-22 07:46:43 +0200

    small adjusments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,14 @@ import { Component, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { SocketService } from './socket.service';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 interface UserData {
   name: string;
   color: string;
   badge: string | null;
+  sub: boolean;
+  mirror: boolean;
   xPosition: string;
   messages: {
     quote: string | null;
@@ -19,7 +22,7 @@ interface UserData {
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   standalone: true,
-  imports: [FormsModule],
+  imports: [CommonModule, FormsModule],
 })
 export class AppComponent implements OnDestroy {
   private messageSubscription: Subscription;
@@ -34,7 +37,7 @@ export class AppComponent implements OnDestroy {
         if (idx > -1) {
           this.chat[idx].messages.push(data.message);
           // TODO pop up in front
-          let overChating = this.chat[idx].messages.length - 10;
+          let overChating = this.chat[idx].messages.length - 5;
           for (var i = 0; i < overChating; i++) {
             document.querySelector('.user-message')?.remove();
             this.chat[idx].messages.shift();
@@ -46,10 +49,18 @@ export class AppComponent implements OnDestroy {
             name: data.user,
             color: data.color,
             badge: data.badge,
+            sub: data.badge && (
+              data.badge.search('Fondateur') > -1 ||
+              data.badge.search('Modérateur') > -1 ||
+              data.badge.search('Vérifié') > -1 ||
+              data.badge.search('Abonn') > -1 ||
+              data.badge.search('cheer') > -1
+              ),
             xPosition: `${Math.round(Math.random() * 84)}vw`,
+            mirror: Math.random() > 0.5,
             messages: [data.message]
           });
-          let overPopulation = this.users.length - 100;
+          let overPopulation = this.users.length - 50;
           for (var i = 0; i < overPopulation; i++) {
             document.querySelector('.user-container')?.remove();
             this.users.shift();
